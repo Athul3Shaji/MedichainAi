044157f39dc000467aa721526f133dbad8e8034f
Athul3Shaji <athulshaji125@gmail.com>
2025-05-24 16:49:46 +0530

    first commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,4 +5,8 @@ const { registerUser, loginUser } = require('../controllers/userController');
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 
+<<<<<<< HEAD
 module.exports = router;
+=======
+module.exports = router; 
+>>>>>>> e1fe2e3 (first commit)
